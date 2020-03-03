f647362368ff2fb550ec8a0209e52736ef1f5c78
root <root@DESKTOP-2KLJLBF.localdomain>
2020-03-03 17:06:33 -0600

    Heads of state of Sweden not auto populate through Javascript.

---
+++ scripts/sweden/leaders/LeaderDataProvider.js
@@ -0,0 +1,18 @@
+const swedishLeaders = [
+    {
+    name:"Silvia Sommerlath",
+    title: "Queen"
+    },
+    {
+    name:"King Carl XVI Gustaf",
+    title: "King"
+    },
+    {
+    name:"Stefan Löfven",
+    title: "Prime Minister"
+    },
+]
+
+export const useLeaders = () => {
+    return swedishLeaders.slice()
+}
